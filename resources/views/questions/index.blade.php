c03f7d988e11cd910988db77b9ff1bf72717fda0
hrzqrz <hrzqrz@gmail.com>
2020-04-13 09:12:43 +0430

    Add votes, answers and views counter to question item

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,6 +10,19 @@
                 <div class="card-body">
                     @foreach( $questions as $question)
                         <div class="media mb-3">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{$question->votes}} &nbsp;</strong>{{str_plural('vote', $question->votes)}}
+                                </div>
+
+                            <div class="status {{ $question->status }}">
+                                    <strong>{{$question->answers}} &nbsp;</strong>{{str_plural('answer', $question->answers)}}
+                                </div>
+
+                                <div class="view">
+                                    {{$question->views." ".str_plural('view', $question->views)}}
+                                </div>
+                            </div>
                             <div class="media-body">
                                 <h3 class="mt-3"> <a href="{{$question->url}}">{{$question->title}}</a> </h3>
                                 <p class="lead">
@@ -19,8 +32,9 @@
                                 </p>
                                 {{str_limit($question->body, 250)}}
                             </div>
-                            <hr>
+                            
                         </div>
+                        <hr>
                     @endforeach
                     <div class="paginqation justify-content-center">
                         {{$questions->links()}}
