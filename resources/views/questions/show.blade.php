416e99323cae4302243dfa2edb85f4b9004291df
hrzqrz <hrzqrz@gmail.com>
2020-04-22 08:22:34 +0430

    Add vote controls to question and answer item

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,26 +5,48 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h1>{{$question->title}}</h1>
-                        <div class="ml-auto">
-                            <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all
-                                questions</a>
-                        </div>
-                    </div>
-                </div>
-
                 <div class="card-body">
-                    {!! $question->body_html !!}
-                    <div class="float-right">
-                        <span class="text-muted">Asked: {{$question->created_date}}</span>
-                        <div class="media mt-2">
-                            <a href="{{$question->user->url}}" class="pr-2">
-                                <img src="{{$question->user->avatar}}" alt="">
+                    <div class="card-title">
+                        <div class="d-flex align-items-center">
+                            <h1>{{$question->title}}</h1>
+                            <div class="ml-auto">
+                                <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all
+                                    questions</a>
+                            </div>
+                        </div>
+                        <hr>
+                    </div>
+
+                    <div class="media">
+                        {{-- برای اضافه کردن این ایکن ها از لینک زیر استفاده می کنیم  --}}
+                        {{-- این لینک را باید به فایل app.blade.php  اضافه می کنیم  --}}
+                        {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> --}}
+                        <div class="d-flex flex-column vote-controls">
+                            <a title="This question is usefull" class="vote-up">
+                                <i class="fa fa-caret-up fa-3x"></i>
                             </a>
-                            <div class="media-body mt-1">
-                                <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                            <span class="votes-count">1230</span>
+                            <a title="This question is not useful" class="vote-down">
+                                <i class="fa fa-caret-down fa-3x"></i>
+                            </a>
+                            <a title="Click to mark as favorite question (Click again to undo )"
+                                class="favorite mt-2 favorited">
+                                <i class="fa fa-star fa-2x"></i>
+                                <span class="favorites-count">123</span>
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+                            <div class="float-right">
+                                <span class="text-muted">Asked: {{$question->created_date}}</span>
+                                <div class="media mt-2">
+                                    <a href="{{$question->user->url}}" class="pr-2">
+                                        <img src="{{$question->user->avatar}}" alt="">
+                                    </a>
+                                    <div class="media-body mt-1">
+                                        <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -42,6 +64,20 @@
                     <hr>
                     @foreach( $question->answers as $answer )
                     <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a title="This answer is usefull" class="vote-up">
+                                <i class="fa fa-caret-up fa-3x"></i>
+                            </a>
+                            <span class="votes-count">1230</span>
+                            <a title="This answer is not useful" class="vote-down">
+                                <i class="fa fa-caret-down fa-3x"></i>
+                            </a>
+                            <a title="Mark this answer as best answer"
+                                class="vote-accetped mt-2 ">
+                                <i class="fa fa-check fa-2x"></i>
+                                <span class="favorites-count">123</span>
+                            </a>
+                        </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
                             <div class="float-right">
